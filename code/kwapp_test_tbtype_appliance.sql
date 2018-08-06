4fbd7c0a171a8f2c9d54d4b33a3a0f1d3c8832e7
Eminjan <18069585+EminjanO@users.noreply.github.com>
2018-08-06 10:39:38 +0200

    insert appliance, typeappliences

---
+++ code/kwapp_test_tbtype_appliance.sql
@@ -0,0 +1,8 @@
+INSERT INTO kwapp_test.tbtype_appliance (idType_Appliance, type_ApplianceName) VALUES (1, 'Kitchen');
+INSERT INTO kwapp_test.tbtype_appliance (idType_Appliance, type_ApplianceName) VALUES (2, 'Heating/Cooling');
+INSERT INTO kwapp_test.tbtype_appliance (idType_Appliance, type_ApplianceName) VALUES (3, 'Laundry');
+INSERT INTO kwapp_test.tbtype_appliance (idType_Appliance, type_ApplianceName) VALUES (4, 'Living Room');
+INSERT INTO kwapp_test.tbtype_appliance (idType_Appliance, type_ApplianceName) VALUES (5, 'Lights');
+INSERT INTO kwapp_test.tbtype_appliance (idType_Appliance, type_ApplianceName) VALUES (6, 'Office');
+INSERT INTO kwapp_test.tbtype_appliance (idType_Appliance, type_ApplianceName) VALUES (7, 'Tools');
+INSERT INTO kwapp_test.tbtype_appliance (idType_Appliance, type_ApplianceName) VALUES (8, 'Misc');
